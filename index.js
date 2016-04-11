136e9721edaef5dcfcc9cb484c53b57eb1b3c37d
polygoning <andrewclarke540@gmail.com>
2016-04-11 00:25:42 -0400

    Add functionality and test for unique 'short_url's/finish project

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 var express = require('express');
 var app = express();
+var validUrl = require('valid-url');
 
-var Router = require('name-routes');
-var router = new Router();
-router.extendExpress(app);
-router.registerAppHelpers(app);
+var url = {
+  "1234": "https://www.google.com/"
+};
 
+app.get('/:url', function(req, res){
+  if (url[req.params.url]) {
+    res.redirect(url[req.params.url]);
+  }
+  else {
+    res.send("Sorry that url is not in the database.");
+  }
+  res.end();
+});
+
+app.get(/^\/new\/(\w+)(.*?)/, function(req, res) {
+  var unique = false;
+  var reqUrl = req.path.substr(5);
+  if (validUrl.isUri(reqUrl)) {
+    var randomDigits = randomDigits = Math.floor(100000 + Math.random() * 900000);
+    randomDigits = randomDigits.toString().substring(0, 4);
+    randomDigits = "1234";
+    while (unique == false) {
+      if (url[randomDigits]) {
+        randomDigits = Math.floor(100000 + Math.random() * 900000);
+        randomDigits = randomDigits.toString().substring(0, 4);
+      }
+      else {
+        unique = true
+      }
+    }
+    var returnObj = {
+      "original_url": reqUrl,
+      "short_url": req.get('host') + "/" + randomDigits
+    };
+    url[randomDigits] = reqUrl;
+    res.send(JSON.stringify(returnObj));
+  }
+  else {
+    res.send('Not a valid URL');
+  }
+  res.end();
+});
+app.use('/', express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
-  res.redirect("https://www.google.com");
   res.end();
 });
 
-app.get('/new/:url', function(req, res) {
-
-  var returnObj = {
-    "original_url": req.originalUrl,
-    "short_url": null
-  };
-
-  res.send(JSON.stringify(returnObj));
-
-  res.end();
-});
 app.listen(3000, function() {console.log('Server is running on port 3000...')});
